1bd793633fe1d2c0a14872c84e8b30d3cb1ede2f
healim01 <22000770@handong.edu>
2023-08-04 03:40:09 +0900

    :sparkles: Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import { styled } from 'styled-components';
+import logo from '../imgs/logoQuery.png';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import { Link } from 'react-router-dom';
+
+const TopHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  height: 70px;
+  background-color: white;
+`;
+
+const Logo = styled.img`
+  width: 60px;
+`;
+
+export default function Header() {
+  return (
+    <>
+      <TopHeader>
+        <Link to="/">
+          <Logo src={logo} />
+        </Link>
+        <Link to="/todo">
+          <NotificationsNoneOutlinedIcon />
+        </Link>
+      </TopHeader>
+    </>
+  );
+}
