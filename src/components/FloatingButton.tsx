8b0f93831f1a35ece67d0f9ceeb4bed4c042d640
healim01 <22000770@handong.edu>
2023-08-04 03:40:42 +0900

    :sparkles: floating

---
+++ src/components/FloatingButton.tsx
@@ -0,0 +1,29 @@
+import { Fab } from '@mui/material';
+import { Add } from '@mui/icons-material';
+import { styled } from 'styled-components';
+
+const Float = styled.div`
+  position: fixed;
+  bottom: 10%;
+  right: 20px;
+  z-index: 100;
+`;
+
+// Define your custom gray color here
+const CustomFab = styled(Fab)`
+  background-color: gray;
+  /* Add any additional styling you need */
+`;
+
+export default function FloatingButton() {
+  return (
+    <>
+      <Float>
+        {/* Use the CustomFab instead of the Fab */}
+        <CustomFab aria-label="add">
+          <Add />
+        </CustomFab>
+      </Float>
+    </>
+  );
+}
