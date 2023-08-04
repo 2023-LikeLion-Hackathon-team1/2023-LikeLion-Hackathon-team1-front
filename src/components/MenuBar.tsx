6c4b6ccfa145b52230e16d2d0ff460aca311fd45
healim01 <22000770@handong.edu>
2023-08-05 01:43:56 +0900

    :lipstick: style: change size

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -11,33 +11,34 @@ const BottomBar = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   gap: 10px;
-  align-items: center;
+  align-items: start;
   justify-items: center;
   position: fixed;
   bottom: 0;
   min-width: 100%;
-  height: 60px;
+  height: 70px;
   color: black;
   padding: 10px;
   background-color: white;
-  margin-bottom: 10px;
 `;
 
 export default function MenuBar() {
   return (
-    <BottomBar>
-      <Link to="/">
-        <HomeOutlinedIcon />
-      </Link>
-      <Link to="/todo">
-        <BookmarkBorderOutlinedIcon />
-      </Link>
-      <Link to="/todo">
-        <SearchOutlinedIcon />
-      </Link>
-      <Link to="/todo">
-        <PersonOutlineOutlinedIcon />
-      </Link>
-    </BottomBar>
+    <>
+      <BottomBar>
+        <Link to="/">
+          <HomeOutlinedIcon />
+        </Link>
+        <Link to="/todo">
+          <BookmarkBorderOutlinedIcon />
+        </Link>
+        <Link to="/todo">
+          <SearchOutlinedIcon />
+        </Link>
+        <Link to="/todo">
+          <PersonOutlineOutlinedIcon />
+        </Link>
+      </BottomBar>
+    </>
   );
 }
