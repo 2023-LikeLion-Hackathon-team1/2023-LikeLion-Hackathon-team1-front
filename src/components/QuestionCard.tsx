c061eace7611756fde37ce8d5f0dc0fbee627e30
healim01 <22000770@handong.edu>
2023-08-05 05:22:53 +0900

    :sparkles: change props

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -4,114 +4,8 @@ import { styled } from 'styled-components';
 import PsychologyAltOutlinedIcon from '@mui/icons-material/PsychologyAltOutlined';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
-import { QuestionMark } from '@mui/icons-material';
 
-const questions: Question[] = [
-  {
-    questionId: 1,
-    categoryId: 1,
-    userName: 'lullu',
-    userImg: 'https://picsum.photos/200',
-    title: '다들 디자인 시스템 어디서 참고하나요?',
-    content: '아무리 찾아봐도.. 어디서 레퍼런스 찾기가 쉽지 않네요ㅜㅜㅜㅜ 어디서 참고하시나요? 아시는 분 알려주세요!',
-    questionMark: 10,
-    answerNum: 3,
-    postTime: new Date('2023-08-04 10:10:10'),
-  },
-  {
-    questionId: 2,
-    categoryId: 1,
-    userName: 'jane_doe',
-    userImg: 'https://picsum.photos/201',
-    title: '리액트 vs. 뷰: 초보자에게 더 나은 것은 무엇인가요?',
-    content: '웹 개발을 처음 시작하는데 초보자 친화적인 프레임워크로 리액트와 뷰 중 어떤 것을 선택해야 할까요?',
-    questionMark: 5,
-    answerNum: 7,
-    postTime: new Date('2023-08-05 01:30:45'),
-  },
-  {
-    questionId: 3,
-    categoryId: 1,
-    userName: 'dev_guy',
-    userImg: 'https://picsum.photos/203',
-    title: '웹사이트 성능 최적화하는 방법은 무엇인가요?',
-    content: '내 웹사이트가 느리게 로딩되고 있어요. 성능을 향상시키는 방법이 있을까요? 팁이나 권장 사항이 있나요?',
-    questionMark: 8,
-    answerNum: 12,
-    postTime: new Date('2022-09-02 08:20:55'),
-  },
-  {
-    questionId: 4,
-    categoryId: 1,
-    userName: 'coding_master',
-    userImg: 'https://picsum.photos/204',
-    title: '고급 CSS 트릭은 어떤 게 있나요?',
-    content: 'CSS 기술을 더욱 발전시키고 싶습니다. 잘 알려지지 않았지만 강력한 CSS 트릭을 공유해 주시겠어요?',
-    questionMark: 7,
-    answerNum: 9,
-    postTime: new Date('2022-11-20 12:05:30'),
-  },
-  {
-    questionId: 5,
-    categoryId: 1,
-    userName: 'design_guru',
-    userImg: 'https://picsum.photos/205',
-    title: '주목할만한 일러스트레이션을 만드는 팁은 무엇인가요?',
-    content:
-      '주목을 끌 수 있는 일러스트레이션을 만들고 싶습니다. 흥미로운 작품을 만드는 데 도움이 되는 조언이나 자료가 있나요?',
-    questionMark: 6,
-    answerNum: 4,
-    postTime: new Date('2022-07-07 17:40:20'),
-  },
-  {
-    questionId: 6,
-    categoryId: 1,
-    userName: 'data_whiz',
-    userImg: 'https://picsum.photos/206',
-    title: '데이터 분석을 위한 최상의 방법은 무엇인가요?',
-    content:
-      '대규모 데이터셋에 뛰어들려고 합니다. 데이터 분석이 견고하고 정확하도록 하기 위한 최상의 방법이 무엇인가요?',
-    questionMark: 9,
-    answerNum: 11,
-    postTime: new Date('2022-12-12 09:15:05'),
-  },
-  {
-    questionId: 7,
-    categoryId: 1,
-    userName: 'gaming_addict',
-    userImg: 'https://picsum.photos/207',
-    title: '역대 최고의 RPG 게임은 무엇인가요?',
-    content: '롤플레잉 게임(RPG)에 큰 관심이 있습니다. 역대 최고의 RPG 게임 몇 가지를 추천해 주시겠어요?',
-    questionMark: 8,
-    answerNum: 15,
-    postTime: new Date('2022-06-30 22:55:40'),
-  },
-  {
-    questionId: 8,
-    categoryId: 1,
-    userName: 'travel_lover',
-    userImg: 'https://picsum.photos/208',
-    title: '남미에서 가볼만한 숨은 보석지는 어디인가요?',
-    content: '남미 여행을 계획 중이고 비인기 여행지를 탐험하고 싶습니다. 어떤 곳을 추천해주시나요?',
-    questionMark: 7,
-    answerNum: 6,
-    postTime: new Date('2023-01-05 11:25:50'),
-  },
-  {
-    questionId: 9,
-    categoryId: 1,
-    userName: 'fitness_enthusiast',
-    userImg: 'https://picsum.photos/209',
-    title: '운동기구 없이 효과적인 유산소 운동은 무엇인가요?',
-    content:
-      '유산소 운동 능력을 향상시키고 싶지만 체육관에 가지 못합니다. 기구 없이 집에서 할 수 있는 효과적인 유산소 운동이 어떤 것이 있나요?',
-    questionMark: 6,
-    answerNum: 8,
-    postTime: new Date('2023-04-18 16:30:15'),
-  },
-];
-
-interface Question {
+interface Questions {
   questionId: number;
   categoryId: number;
   userName: string;
@@ -124,12 +18,14 @@ interface Question {
 }
 
 interface QuestionCardProps {
-  questionId: number;
+  question: Questions;
+  isSummary: boolean;
 }
 
 const Card = styled.div`
-  width: 100%;
-  height: 140px;
+  width: 100vw;
+  /* min-width: 100vw; */
+  /* height: 140px; */
   /* background-color: lightblue; */
   /* border-top: 1px solid black; */
   border-bottom: 1px solid lightgray;
@@ -201,19 +97,19 @@ const Response = styled.div`
 `;
 const Answer = styled.div``;
 
-export default function QuestionCard({ questionId }: QuestionCardProps) {
+export default function QuestionCard({ question, isSummary }: QuestionCardProps) {
   const [timeAgo, setTimeAgo] = useState<string>('');
 
   useEffect(() => {
     const currentDate = new Date();
-    const timeDifference = currentDate.getTime() - questions[questionId - 1].postTime.getTime();
+    const timeDifference = currentDate.getTime() - question.postTime.getTime();
     const hoursAgo = Math.floor(timeDifference / (1000 * 60 * 60));
     const minutesAgo = Math.floor(timeDifference / (1000 * 60));
     const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
     let timeAgoStr = '';
     if (daysAgo >= 1) {
-      const formattedDate = questions[questionId - 1].postTime.toLocaleDateString('en-US', {
+      const formattedDate = question.postTime.toLocaleDateString('en-US', {
         month: '2-digit',
         day: '2-digit',
       });
@@ -225,22 +121,20 @@ export default function QuestionCard({ questionId }: QuestionCardProps) {
     }
 
     setTimeAgo(timeAgoStr);
-  }, [questions[questionId - 1].postTime]);
+  }, [question.postTime]);
 
   return (
     <Card>
       <Profile>
-        <ProfileImg src={questions[questionId - 1].userImg} />
+        <ProfileImg src={question.userImg} />
         <ProfileColumn>
-          <ProfileName>{questions[questionId - 1].userName}</ProfileName>
+          <ProfileName>{question.userName}</ProfileName>
           <Time>{timeAgo}</Time>
         </ProfileColumn>
       </Profile>
-      <Title> {questions[questionId - 1].title} </Title>
+      <Title> {question.title} </Title>
       <Content>
-        {questions[questionId - 1].content.length > 30
-          ? questions[questionId - 1].content.slice(0, 30) + '...'
-          : questions[questionId - 1].content}
+        {question.content.length > 30 && isSummary === true ? question.content.slice(0, 30) + '...' : question.content}
       </Content>
       <Bottom>
         <Icons>
@@ -249,8 +143,8 @@ export default function QuestionCard({ questionId }: QuestionCardProps) {
           <BookmarkBorderOutlinedIcon />
         </Icons>
         <Response>
-          <Answer>{`궁금해요 ${questions[questionId - 1].questionMark}개`} </Answer>
-          <Answer>{`답변 ${questions[questionId - 1].answerNum}개`} </Answer>
+          <Answer>{`궁금해요 ${question.questionMark}개`} </Answer>
+          <Answer>{`답변 ${question.answerNum}개`} </Answer>
         </Response>
       </Bottom>
     </Card>
--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -6,35 +6,36 @@ import { Link } from 'react-router-dom';
 import CategoryMenuBar from '../components/CategoryMenuBar';
 import QuestionCard from '../components/QuestionCard';
 import FloatingButton from '../components/FloatingButton';
+import { Divider } from '@mui/material';
 
-const questions: Question[] = [
+const questions: Questions[] = [
   {
     questionId: 1,
     categoryId: 1,
     userName: 'lullu',
-    userImg: 'https://picsum.photos/200/1',
+    userImg: 'https://picsum.photos/200',
     title: '다들 디자인 시스템 어디서 참고하나요?',
     content: '아무리 찾아봐도.. 어디서 레퍼런스 찾기가 쉽지 않네요ㅜㅜㅜㅜ 어디서 참고하시나요? 아시는 분 알려주세요!',
     questionMark: 10,
     answerNum: 3,
-    postTime: new Date('2021-10-10 10:10:10'),
+    postTime: new Date('2023-08-04 10:10:10'),
   },
   {
     questionId: 2,
     categoryId: 1,
     userName: 'jane_doe',
-    userImg: 'https://picsum.photos/200/2',
+    userImg: 'https://picsum.photos/201',
     title: '리액트 vs. 뷰: 초보자에게 더 나은 것은 무엇인가요?',
     content: '웹 개발을 처음 시작하는데 초보자 친화적인 프레임워크로 리액트와 뷰 중 어떤 것을 선택해야 할까요?',
     questionMark: 5,
     answerNum: 7,
-    postTime: new Date('2022-05-15 15:30:45'),
+    postTime: new Date('2023-08-05 01:30:45'),
   },
   {
     questionId: 3,
     categoryId: 1,
     userName: 'dev_guy',
-    userImg: 'https://picsum.photos/200/3',
+    userImg: 'https://picsum.photos/203',
     title: '웹사이트 성능 최적화하는 방법은 무엇인가요?',
     content: '내 웹사이트가 느리게 로딩되고 있어요. 성능을 향상시키는 방법이 있을까요? 팁이나 권장 사항이 있나요?',
     questionMark: 8,
@@ -45,7 +46,7 @@ const questions: Question[] = [
     questionId: 4,
     categoryId: 1,
     userName: 'coding_master',
-    userImg: 'https://picsum.photos/200/4',
+    userImg: 'https://picsum.photos/204',
     title: '고급 CSS 트릭은 어떤 게 있나요?',
     content: 'CSS 기술을 더욱 발전시키고 싶습니다. 잘 알려지지 않았지만 강력한 CSS 트릭을 공유해 주시겠어요?',
     questionMark: 7,
@@ -56,7 +57,7 @@ const questions: Question[] = [
     questionId: 5,
     categoryId: 1,
     userName: 'design_guru',
-    userImg: 'https://picsum.photos/200/5',
+    userImg: 'https://picsum.photos/205',
     title: '주목할만한 일러스트레이션을 만드는 팁은 무엇인가요?',
     content:
       '주목을 끌 수 있는 일러스트레이션을 만들고 싶습니다. 흥미로운 작품을 만드는 데 도움이 되는 조언이나 자료가 있나요?',
@@ -68,7 +69,7 @@ const questions: Question[] = [
     questionId: 6,
     categoryId: 1,
     userName: 'data_whiz',
-    userImg: 'https://picsum.photos/200/6',
+    userImg: 'https://picsum.photos/206',
     title: '데이터 분석을 위한 최상의 방법은 무엇인가요?',
     content:
       '대규모 데이터셋에 뛰어들려고 합니다. 데이터 분석이 견고하고 정확하도록 하기 위한 최상의 방법이 무엇인가요?',
@@ -80,7 +81,7 @@ const questions: Question[] = [
     questionId: 7,
     categoryId: 1,
     userName: 'gaming_addict',
-    userImg: 'https://picsum.photos/200/7',
+    userImg: 'https://picsum.photos/207',
     title: '역대 최고의 RPG 게임은 무엇인가요?',
     content: '롤플레잉 게임(RPG)에 큰 관심이 있습니다. 역대 최고의 RPG 게임 몇 가지를 추천해 주시겠어요?',
     questionMark: 8,
@@ -91,7 +92,7 @@ const questions: Question[] = [
     questionId: 8,
     categoryId: 1,
     userName: 'travel_lover',
-    userImg: 'https://picsum.photos/200/8',
+    userImg: 'https://picsum.photos/208',
     title: '남미에서 가볼만한 숨은 보석지는 어디인가요?',
     content: '남미 여행을 계획 중이고 비인기 여행지를 탐험하고 싶습니다. 어떤 곳을 추천해주시나요?',
     questionMark: 7,
@@ -102,7 +103,7 @@ const questions: Question[] = [
     questionId: 9,
     categoryId: 1,
     userName: 'fitness_enthusiast',
-    userImg: 'https://picsum.photos/200/9',
+    userImg: 'https://picsum.photos/209',
     title: '운동기구 없이 효과적인 유산소 운동은 무엇인가요?',
     content:
       '유산소 운동 능력을 향상시키고 싶지만 체육관에 가지 못합니다. 기구 없이 집에서 할 수 있는 효과적인 유산소 운동이 어떤 것이 있나요?',
@@ -111,16 +112,7 @@ const questions: Question[] = [
     postTime: new Date('2023-04-18 16:30:15'),
   },
 ];
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  /* padding-top: 20px; */
-`;
-
-interface Question {
+interface Questions {
   questionId: number;
   categoryId: number;
   userName: string;
@@ -131,6 +123,14 @@ interface Question {
   answerNum: number;
   postTime: Date;
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0px;
+`;
 
 export default function Main() {
   return (
@@ -144,7 +144,9 @@ export default function Main() {
       <CategoryMenuBar />
       <Container>
         {questions.map((question) => (
-          <QuestionCard questionId={question.questionId} />
+          <Link to={`/${question.categoryId}/${question.questionId}`} style={{ margin: 0, padding: 0 }}>
+            <QuestionCard question={question} isSummary={true} />
+          </Link>
         ))}
       </Container>
       <FloatingButton />
